71e4da86379a5c134713fa86bc53563c08f7b156
nzi19001@byui.edu <nzi19001@byui.edu>
2024-02-20 22:43:16 +0300

    done assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
@@ -14,7 +16,7 @@ test("renders the correct child components", () => {
     Uncomment the line below to see the DOM elements being returned
     by the App component in your terminal when you run the tests
   */
-  // screen.debug();
+  screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
